7d729c149eee56d57e60ecc65bb49cda46f616a8
Shawn Badger <shawnbadger@gmail.com>
2024-08-06 19:17:00 -0600

    Fix issues on Ubuntu, and make it easy to modify capslock behavior

--- linux/activate.sh
+++ linux/activate.sh
@@ -7,6 +7,9 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 LAYOUT=custom
 VARIANT=$1  # user-specified variant to activate
 
+#OPTION_CAPS=backspace         # Uncomment to make capslock a backspace
+#OPTION_CAPS=ctrl_modifier    # Uncomment to make capslock a ctrl modifier
+
 # Get an array of the variants available
 VARIANTS=$(find $SCRIPT_DIR/xkb -type f ! -name "evdev.xml" -exec basename '{}' \; | xargs)
 BUILTINS=" dvorak colemak colemak_dh workman qwerty"
@@ -43,24 +46,27 @@ if test ! -z "$IS_BUILTIN"; then
     fi
 fi
 
-if [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]; then
+if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]] || [[ "$XDG_CURRENT_DESKTOP" == ubuntu* ]]; then
     if test -z $VARIANT; then
         gsettings set org.gnome.desktop.input-sources sources "[('xkb', '${LAYOUT}')]"
     else
         gsettings set org.gnome.desktop.input-sources sources "[('xkb', '${LAYOUT}+${VARIANT}')]"
     fi
 
-    #gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+    if test ! -z ${OPTION_CAPS}; then
+        gsettings set org.gnome.desktop.input-sources xkb-options "['caps:${OPTION_CAPS}']"
+    fi
 
-elif [ "$XDG_CURRENT_DESKTOP" == "KDE" ]; then
+elif [[ "$XDG_CURRENT_DESKTOP" == "KDE" ]]; then
     kwriteconfig6 --file kxkbrc --group Layout --key LayoutList $LAYOUT
     kwriteconfig6 --file kxkbrc --group Layout --key VariantList $VARIANT
-    #kwriteconfig6 --file kxkbrc --group Layout --key Options caps:backspace # Uncomment for CAPS to be BKSP
-    #kwriteconfig6 --file kxkbrc --group Layout --key Options ctrl:nocaps    # Uncomment for CAPS to be CTRL
+    if test ! -z ${OPTION_CAPS}; then
+        kwriteconfig6 --file kxkbrc --group Layout --key Options caps:${OPTION_CAPS}
+    fi
     dbus-send --session --type=signal --reply-timeout=100 --dest=org.kde.keyboard /Layouts org.kde.keyboard.reloadConfig
 
 else
-    if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+    if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
         echo "You're running Wayland with a compositor other than Gnome or KDE"
         echo "I don't know how to enable the layout for your compositor."
         echo "You will need to manually enable it in your window manager."
@@ -68,10 +74,14 @@ else
     else
         if test -z $VARIANT; then
             setxkbmap $LAYOUT
-            #setxkbmap $LAYOUT -option ctrl:nocaps
+            if test ! -z ${OPTION_CAPS}; then
+                setxkbmap $LAYOUT -option caps:${OPTION_CAPS}
+            fi
         else
             setxkbmap $LAYOUT -variant $VARIANT
-            #setxkbmap $LAYOUT -variant $VARIANT -option ctrl:nocaps
+            if test ! -z ${OPTION_CAPS}; then
+                setxkbmap $LAYOUT -variant $VARIANT -option caps:${OPTION_CAPS}
+            fi
         fi
     fi
 fi
